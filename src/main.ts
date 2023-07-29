a00db2e9e5736e65040d86de40c045b43cf55a13
bigfat <MY_NAME@example.com>
2023-07-30 06:58:38 +0800

    style:删除没使用的包引入

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
-import store from './store'
 import router from './router'
 import App from './App.vue'
-import i18n from './locales/i18n'
 import 'normalize.css'
 import './style.css'
 import 'element-plus/dist/index.css'
 
-createApp(App).use(i18n).use(store).use(router).use(ElementPlus).mount('#app')
+createApp(App).use(router).use(ElementPlus).mount('#app')
