55bfa3db479ace061c411a4a45cf85ce25177268
bigfat <xx@qq.com>
2023-11-09 13:58:27 +0800

    feat:扣除手续费

--- src/views/SwardpoolBeta/computePrice.ts
+++ src/views/SwardpoolBeta/computePrice.ts
@@ -2,10 +2,10 @@ import NP from 'number-precision'
 
 export const calculateOutA = (inputB: number, balanceA: number, balanceB: number, price = 0) => {
   const o_a = NP.times(NP.round(NP.times(inputB, NP.divide(balanceA, balanceB)), price))
-  return o_a
+  return NP.times(o_a, 1 - 0.01)
 }
 
 export const calculateOutB = (inputA: number, balanceA: number, balanceB: number, price = 0) => {
   const o_b = NP.round(NP.times(inputA, NP.divide(balanceB, balanceA)), price)
-  return o_b
+  return NP.times(o_b, 1 - 0.01)
 }
