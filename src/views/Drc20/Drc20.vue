fa4248ddcd0cc5a9fe0a76f322ec58ca521b9f3a
chengpeng <xxxxx@qq.com>
2023-12-31 18:13:36 +0800

    feat:搜索转码

--- src/views/Drc20/Drc20.vue
+++ src/views/Drc20/Drc20.vue
@@ -24,9 +24,9 @@ async function search() {
   if (!tick.value || loadingSearch.value) return
 
   if (tick.value.length > 6) {
-    pushRoute(`/drc20/wallet/${tick.value}`, ['/drc20/wallet', '/drc20/item'])
+    pushRoute(`/drc20/wallet/${decodeURIComponent(tick.value)}`, ['/drc20/wallet', '/drc20/item'])
   } else {
-    pushRoute(`/drc20/item/${tick.value}`, ['/drc20/item'])
+    pushRoute(`/drc20/item/${decodeURIComponent(tick.value)}`, ['/drc20/item'])
   }
 }
 </script>
