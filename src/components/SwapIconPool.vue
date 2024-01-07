c54a22d85b4b853ecbbd690981cea872e10e33ac
chengpeng <xxxxx@qq.com>
2024-01-07 22:46:54 +0800

    feat:图标修改

--- src/components/SwapIconPool.vue
+++ src/components/SwapIconPool.vue
@@ -1,5 +1,6 @@
 <script lang="ts">
 import TokenIcons from '@/config/payIcons'
+import { Plus, Minus, Right } from '@element-plus/icons-vue'
 
 export default {
   props: {
@@ -15,13 +16,16 @@ export default {
       type: String,
       required: true,
     },
+    type: {
+      type: Number,
+      required: true,
+    },
   },
   setup(props) {
     const iconASrc = computed(() => TokenIcons[props.iconA])
     const iconBSrc = computed(() => TokenIcons[props.iconB])
     const imgs = ref<any[]>([])
     const tokenSrc = ref<any>('')
-
     if (props.token == props.iconA) {
       imgs.value = [iconASrc.value, iconBSrc.value]
       tokenSrc.value = iconASrc.value
@@ -29,7 +33,6 @@ export default {
       imgs.value = [iconBSrc.value, iconASrc.value]
       tokenSrc.value = iconBSrc.value
     }
-
     return {
       iconASrc,
       iconBSrc,
@@ -37,13 +40,19 @@ export default {
       tokenSrc,
     }
   },
+  components: { Plus, Minus, Right },
 }
 </script>
 
 <template>
-  <div class="flex justify-center items-end">
-    <img class="tokenIcon" :class="[tokenSrc == s && 'tokenIcon--top']" :src="s" v-for="s in imgs" :key="s" />
+  <div class="flex justify-center items-center">
+    <div class="tokenIcon flex justify-center items-center tokenIcon--flag" :style="{ background: type == 0 ? 'rgb(64, 180, 105)' : 'rgb(255, 90, 80)' }">
+      <el-icon color="#fff"><Plus v-if="type == 0" /><Minus v-if="type == 1" /></el-icon>
+    </div>
+    <el-icon style="font-size: 18px; margin: 0 6px"><Right /></el-icon>
+    <img class="tokenIcon" :src="tokenSrc" />
   </div>
+  <!-- <slot></slot> -->
 </template>
 
 <style lang="scss" scoped>
@@ -52,11 +61,15 @@ export default {
   width: 24px;
   height: 24px;
   border-radius: 50%;
-  border: 2px solid transparent;
+
   &--top {
     border-color: #fff;
     margin-right: -10px;
     z-index: 1;
+  }
+
+  &--flag {
+    background-color: rgb(255, 194, 0);
   }
 }
 </style>
--- src/components/SwapTypeIcon.tsx
+++ src/components/SwapTypeIcon.tsx
@@ -33,16 +33,18 @@ export default defineComponent({
         return 'ROLLBACK'
       } else if (isLiqType) {
         const token = props.record?.inTokenA > 0 ? props.tokenA : props.tokenB
-        return <SwapIconPool iconA={props.tokenA} iconB={props.tokenB} token={token}></SwapIconPool>
+        return <SwapIconPool iconA={props.tokenA} iconB={props.tokenB} token={token} type={isAddLiq ? 0 : 1} v-slots={{ default: swapType }}></SwapIconPool>
       } else if (swapType == 'DUST') {
         return (
-          <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32">
-            <path
-              d="M320 256a64 64 0 1 1 0 128 64 64 0 0 1 0-128z m384 0a64 64 0 1 1 0 128 64 64 0 0 1 0-128z m-192 32a32 32 0 1 1 0 64 32 32 0 0 1 0-64z m-192 352a64 64 0 1 1 0 128 64 64 0 0 1 0-128z m384 0a64 64 0 1 1 0 128 64 64 0 0 1 0-128z m-192 32a32 32 0 1 1 0 64 32 32 0 0 1 0-64z m192-192a32 32 0 1 1 0 64 32 32 0 0 1 0-64z m-384 0a32 32 0 1 1 0 64 32 32 0 0 1 0-64z m192-32a64 64 0 1 1 0 128 64 64 0 0 1 0-128z"
-              fill="#cdcdcd"
-              p-id="4316"
-            ></path>
-          </svg>
+          <div class="flex flex-col items-center">
+            <svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32">
+              <path
+                d="M320 256a64 64 0 1 1 0 128 64 64 0 0 1 0-128z m384 0a64 64 0 1 1 0 128 64 64 0 0 1 0-128z m-192 32a32 32 0 1 1 0 64 32 32 0 0 1 0-64z m-192 352a64 64 0 1 1 0 128 64 64 0 0 1 0-128z m384 0a64 64 0 1 1 0 128 64 64 0 0 1 0-128z m-192 32a32 32 0 1 1 0 64 32 32 0 0 1 0-64z m192-192a32 32 0 1 1 0 64 32 32 0 0 1 0-64z m-384 0a32 32 0 1 1 0 64 32 32 0 0 1 0-64z m192-32a64 64 0 1 1 0 128 64 64 0 0 1 0-128z"
+                fill="black"
+              ></path>
+            </svg>
+            <span class="text-xs">DUST</span>
+          </div>
         )
       } else {
         return swapType
