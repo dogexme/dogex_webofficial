3afa0033259a9e4e5ff3aa1d2a086ba857629d4d
bigfat <you@example.com>
2024-01-08 17:27:09 +0800

    feat:swap、liq可用性判断

--- src/views/SwardpoolBeta/SwardpoolBeta.vue
+++ src/views/SwardpoolBeta/SwardpoolBeta.vue
@@ -61,6 +61,9 @@ const disabledSwap = computed(() => {
   return !!noticeMessage.value || currentPool.value.status != '0' || balanceA == 0 || balanceB == 0
 })
 
+const liqAvaiable = computed(() => appStore.swordPoolInfo.liq_avaiable)
+const swapAvaiable = computed(() => appStore.swordPoolInfo.swap_avaiable)
+
 async function queryPoolStatus(poolid: string, timer = false) {
   try {
     loading.value = true
@@ -208,8 +211,8 @@ onActivated(() => {
                   <div class="selectToken_bar inline-block">
                     <el-button :loading="isBalanceLoading" :icon="Refresh" circle @click="getBalance(address)" />
                     <!-- 生成环境需增加禁用属性 :disabled="!!noticeMessage" -->
-                    <el-button class="mr-3" type="primary" :disabled="disabledSwap" @click="showSwapDialog = true">Swap</el-button>
-                    <el-button style="margin: 0; background-color: #ba77ff; border: 1px solid #ba77ff" type="warning" :disabled="disabledSwap" @click="showAddPools"
+                    <el-button class="mr-3" type="primary" :disabled="disabledSwap && swapAvaiable" @click="showSwapDialog = true">Swap</el-button>
+                    <el-button style="margin: 0; background-color: #ba77ff; border: 1px solid #ba77ff" type="warning" :disabled="disabledSwap && liqAvaiable" @click="showAddPools"
                       >Liquidity<em class="beta" style="color: #ba77ff">BETA</em></el-button
                     >
                   </div>
